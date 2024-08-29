f15750fb676366e1ab19c3a218d7d7dc050e4042
JulietaRiver <julieta.riverazam@gmail.com>
2024-08-28 20:21:58 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,28 +2,23 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Ensure you have the latest Streamlit version
-st.write(f"Streamlit version: {st.__version__}")
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+fig = px.histogram(car_data, x="odometer") # crear un histograma
+fig.show()
 
-@st.cache_data(ttl=3600)
-def load_data():
-    # Cambia 'data.csv' por la ruta de tu archivo CSV
-    df = pd.read_csv('vehicles_us.csv')
-    return df
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+fig.show()
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+        
 
-# Cargar los datos
-data = load_data()
+if st.button('Mostrar Histograma'):  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-# Mostrar el encabezado
-st.header('Lista de Modelos')
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
 
-# Asegúrate de que la columna 'model' existe en el archivo CSV
-if 'model' in data.columns:
-    # Obtener la lista de modelos
-    models = data['model'].tolist()
-
-    # Mostrar la lista en Streamlit
-    st.write('Modelos disponibles:')
-    st.write(models)
-else:
-    st.error('La columna "model" no se encuentra en el archivo CSV.')
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
